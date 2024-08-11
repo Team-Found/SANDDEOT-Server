7937e0c8a15b712f6d3243be43d0883016dfecfb
gamzamandu <galbimandu1126@gmail.com>
2024-08-11 22:57:54 +0900

    feat: github-blog만 따로 추출하도록 제작

--- RSS/main.py
+++ RSS/main.py
@@ -1,34 +1,10 @@
 import feedparser
-
-from domain import findDomain
-
-target_feeds = {
-    "Apple": "https://www.apple.com/newsroom/rss-feed.rss",
-    "Github Blog": "https://github.blog/feed/",
-    # "Billboard": "https://www.billboard.com/feed/",
-    # "The New York Times - The Daily": "http://rss.art19.com/the-daily",
-    # "Mashable": "http://feeds.mashable.com/mashable",
-    # "Lifehacker": "https://lifehacker.com/feed/rss",
-    # "Cup of Jo": "https://feeds.feedburner.com/blogspot/bboSV",
-    # "Fox News": "https://moxie.foxnews.com/google-publisher/latest.xml",
-    # "france24": "https://www.france24.com/en/rss",
-    # "TIME": "https://feeds.feedburner.com/time/world",
-    # "The Verge": "https://www.theverge.com/rss/frontpage",
-    # "kotteke.org": "https://feeds.kottke.org/main",
-    # "NPR: National Public Radio": "https://feeds.npr.org/1004/rss.xml",
-    # "RT": "https://www.rt.com/rss/news/",
-    # "NDTV": "https://feeds.feedburner.com/ndtvnews-world-news",
-    # "The Sun": "https://www.thesun.co.uk/news/worldnews/feed/",
-    # "The Cipher Brief": "https://www.thecipherbrief.com/feed",
-    # "Raw Story": "https://www.rawstory.com/feeds/world.rss",
-    # "Daily Research Plot": "https://dailyresearchplot.com/feed/",
-    # "The World Association of Newspapers": "https://wan-ifra.org/news/feed/",
-    # "Small Wars Journal Blog": "https://smallwarsjournal.com/rss/blogs"
-}
-
 from bs4 import BeautifulSoup
+from domain import findDomain
+from githubImg import findGithubThumbnail
 
-def findThumbnail(entry):
+
+def findThumbnail(siteName,entry):
     # 1. media_thumbnail에서 썸네일 추출
     if 'media_thumbnail' in entry and entry.media_thumbnail:
         return entry.media_thumbnail[0].get('url')
@@ -59,6 +35,31 @@ def findThumbnail(entry):
     return None
 
 
+target_feeds = {
+    "Apple": "https://www.apple.com/newsroom/rss-feed.rss",
+    "Github Blog": "https://github.blog/feed/",
+    # "Billboard": "https://www.billboard.com/feed/",
+    # "The New York Times - The Daily": "http://rss.art19.com/the-daily",
+    # "Mashable": "http://feeds.mashable.com/mashable",
+    # "Lifehacker": "https://lifehacker.com/feed/rss",
+    # "Cup of Jo": "https://feeds.feedburner.com/blogspot/bboSV",
+    # "Fox News": "https://moxie.foxnews.com/google-publisher/latest.xml",
+    # "france24": "https://www.france24.com/en/rss",
+    # "TIME": "https://feeds.feedburner.com/time/world",
+    # "The Verge": "https://www.theverge.com/rss/frontpage",
+    # "kotteke.org": "https://feeds.kottke.org/main",
+    # "NPR: National Public Radio": "https://feeds.npr.org/1004/rss.xml",
+    # "RT": "https://www.rt.com/rss/news/",
+    # "NDTV": "https://feeds.feedburner.com/ndtvnews-world-news",
+    # "The Sun": "https://www.thesun.co.uk/news/worldnews/feed/",
+    # "The Cipher Brief": "https://www.thecipherbrief.com/feed",
+    # "Raw Story": "https://www.rawstory.com/feeds/world.rss",
+    # "Daily Research Plot": "https://dailyresearchplot.com/feed/",
+    # "The World Association of Newspapers": "https://wan-ifra.org/news/feed/",
+    # "Small Wars Journal Blog": "https://smallwarsjournal.com/rss/blogs"
+}
+
+
 for index, (siteName, url) in enumerate(target_feeds.items()):
     rss_url = url
     feed = feedparser.parse(rss_url)
@@ -70,12 +71,22 @@ for index, (siteName, url) in enumerate(target_feeds.items()):
       print(findDomain(rss_url)+'favicon.ico')
 
     #사이트 이름 추출
-    print(feed.feed.title)
+    siteName = feed.feed.title
+    print('사이트이름' + siteName)
 
     for entry in feed.entries:
         print("Title:", entry.title) #글이름
-        print(entry.link) #글 주소
-        print('썸네일 : ', findThumbnail(entry))
+
+        writingUrl = entry.link
+        print('글링크' +writingUrl)
+
+        if siteName == 'The GitHub Blog': #github-blog만 특별히 썸네일 추출
+          thumbnail = findGithubThumbnail(writingUrl)
+        else:
+          thumbnail =  findThumbnail(siteName,entry)
+        
+        print(thumbnail)
+
         # print(entry)
         print()
     
