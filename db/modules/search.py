1aea95f94a44f4e984b78c8726689ed50c4c3ac6
gamzamandu <galbimandu1126@gmail.com>
2024-08-13 05:28:15 +0900

    serch 제한 해제

--- db/modules/search.py
+++ db/modules/search.py
@@ -5,6 +5,6 @@ from embed.embedModel import embedModel
 sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
 
 async def searchSimilar(target:str ,db): #, db: sqlite3.Cursor
-  settingData = db.execute("""select rssID, titleEb, descriptEb from RSS LIMIT 10""")
+  settingData = db.execute("""select rssID, titleEb, descriptEb from RSS""")
   settingData = settingData.fetchall()
   return await embedModel(target,settingData,4)
