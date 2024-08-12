48e93daf785fc2864a479e3e952715f8bf28ad6f
gamzamandu <galbimandu1126@gmail.com>
2024-08-12 21:16:44 +0900

    addRSS api 완성

--- db/modules/AddRSS.py
+++ db/modules/AddRSS.py
@@ -22,36 +22,51 @@ async def fetch_feed(url: str) -> feedparser.FeedParserDict:
             return feedparser.parse(content)
 
 async def addRSS(url: str, db: sqlite3.Cursor) -> Dict[str, str]:
-  try:
+  # try:
     # 비동기 HTTP 요청으로 RSS 피드를 가져옵니다.
     feed = await fetch_feed(url)
-
     # 파비콘 추출
     favicon = feed.feed.image.href if 'image' in feed.feed else await findDomain(url) + '/favicon.ico'
     
     # 사이트 이름과 URL 추출
-    siteName = feed.feed.title
-    siteUrl = feed.feed.link
+    
+    try:
+      siteName = feed.feed.title
+    except:
+      return {"status": "error", "message": "올바른 링크가 아닙니다"}
+    
+    siteID = db.execute("""SELECT siteID FROM site WHERE siteUrl = ?""",(url,))
+    siteID = siteID.fetchall()
+
+    if len(siteID):
+      return {"status": "success", "pk": siteID}
+    else:
+      db.execute("""INSERT INTO site (siteName,siteUrl,favicon) VALUES (?, ?, ?)""",(siteName,url,favicon))
+      db.connection.commit()  # 변경 사항을 데이터베이스에 저장
+      siteID = db.execute("""SELECT siteID FROM site WHERE siteUrl = ?""",(url,))
+      siteID = siteID.fetchall()
+    
+    print(siteID)
 
     for entry in feed.entries:
-        title = entry.title
-        description = entry.description if 'description' in entry else entry.summary
-        description = ' '.join(str(await htmlToPlaintext(description)).split()[:40])
+      title = entry.title
+      description = entry.description if 'description' in entry else entry.summary
+      description = ' '.join(str(await htmlToPlaintext(description)).split()[:40])
 
-        writingUrl = entry.link  # 글 링크
-        thumbnail = await findImgList(siteName, entry)  # 썸네일과 이미지 리스트 추출
-        published = entry.published if 'published' in entry else None
+      writingUrl = entry.link  # 글 링크
+      thumbnail = await findImgList(siteName, entry)  # 썸네일과 이미지 리스트 추출
+      published = entry.published if 'published' in entry else None
 
-        # 동기적으로 데이터베이스에 삽입
-        db.execute("""
-            INSERT INTO RSS (
-                title, descript, date, siteName, siteUrl, thumbnail, imgList, titleEb, descriptEb
-            ) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)
-        """, (
-            title, description, published, siteName, siteUrl, thumbnail["thumbnail"], json.dumps(thumbnail["imgList"]), await embedding(title), await embedding(description)
-        ))
-        db.connection.commit()  # 변경 사항을 데이터베이스에 저장
+      # 동기적으로 데이터베이스에 삽입
+      db.execute("""
+          INSERT INTO RSS (
+              title, descript, date, thumbnail, imgList, titleEb, descriptEb, siteID
+          ) VALUES (?, ?, ?, ?, ?, ?, ?, ?)
+      """, (
+          title, description, published,thumbnail["thumbnail"], json.dumps(thumbnail["imgList"]), await embedding(title), await embedding(description), int(siteID[0][0])
+      ))
+      db.connection.commit()  # 변경 사항을 데이터베이스에 저장
 
     return {"status": "success", "domain": url}
-  except Exception as e:
-      return {"status": "error", "message": str(e)}
+  # except Exception as e:
+  #     return {"status": "error", "message": str(e)}
