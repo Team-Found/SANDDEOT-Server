386d6cf23f8116c0039182149549f88c3ff3fc38
obtuse-triangle <me@obtuse.kr>
2024-08-16 03:49:27 +0900

    글 불러오기 비동기화

--- db/modules/AddRSS.py
+++ db/modules/AddRSS.py
@@ -12,6 +12,23 @@ from bs4 import BeautifulSoup
 import feedparser
 import sys
 import os
+import time
+import queue
+import threading
+import asyncio
+from db.db import get_db
+
+# 큐 생성
+task_queue = queue.Queue()
+
+
+async def process_queue():
+    while not task_queue.empty():
+        func, entry, rss_id, rss_name = task_queue.get()
+        await func(entry, rss_id, rss_name)
+        task_queue.task_done()
+
+
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 # db_modules/add_rss.py
 
@@ -22,67 +39,104 @@ async def fetch_feed(url: str) -> feedparser.FeedParserDict:
             content = await response.text()
             return feedparser.parse(content)
 
+
 async def addRSS(url: str, db: sqlite3.Cursor) -> Dict[str, str]:
-  # try:
+    # try:
     # 비동기 HTTP 요청으로 RSS 피드를 가져옵니다.
     feed = await fetch_feed(url)
     # 파비콘 추출
-    favicon = feed.feed.image.href if 'image' in feed.feed else await findDomain(url) + '/favicon.ico'
-    
+    favicon = (
+        feed.feed.image.href
+        if "image" in feed.feed
+        else await findDomain(url) + "/favicon.ico"
+    )
+
     # 사이트 이름과 URL 추출
-    
+
     try:
-      rssName = feed.feed.title
+        rssName = feed.feed.title
     except:
-      return {"status": "error", "message": "올바른 링크가 아닙니다"}
-    
-    rssID = db.execute("""SELECT * FROM rss WHERE rssUrl = ?""",(url,))
+        return {"status": "error", "message": "올바른 링크가 아닙니다"}
+
+    rssID = db.execute("""SELECT * FROM rss WHERE rssUrl = ?""", (url,))
     rssID = rssID.fetchall()
 
     if len(rssID):
-      return {"status": "success", 
-              "rssID": rssID[0][0], 
-              "rssName": rssID[0][1], 
-              "rssUrl": rssID[0][2], 
-              "favicon": rssID[0][3], 
-              "message": "already created"}
+        return {
+            "status": "success",
+            "rssID": rssID[0][0],
+            "rssName": rssID[0][1],
+            "rssUrl": rssID[0][2],
+            "favicon": rssID[0][3],
+            "message": "already created",
+        }
     else:
-      db.execute("""INSERT INTO rss (rssName,rssUrl,favicon) VALUES (?, ?, ?)""",(rssName,url,favicon))
-      db.connection.commit()  # 변경 사항을 데이터베이스에 저장
-      rssID = db.execute("""SELECT * FROM rss WHERE rssUrl = ?""",(url,))
-      rssID = rssID.fetchall()
-    
-    # print(rssID)
+        db.execute(
+            """INSERT INTO rss (rssName,rssUrl,favicon) VALUES (?, ?, ?)""",
+            (rssName, url, favicon),
+        )
+        db.connection.commit()  # 변경 사항을 데이터베이스에 저장
+        rssID = db.execute("""SELECT * FROM rss WHERE rssUrl = ?""", (url,))
+        rssID = rssID.fetchall()
 
     for entry in feed.entries:
-      title = entry.title
-      description = entry.description if 'description' in entry else entry.summary
-      description = ' '.join(str(await htmlToPlaintext(description)).split()[:40])
-      if description == None:
+        task_queue.put((insertRssArticle, entry, rssID[0][0], rssName))
+    # 큐 처리 스레드 시작
+    loop = asyncio.new_event_loop()
+    asyncio.set_event_loop(loop)
+    thread = threading.Thread(target=loop.run_until_complete, args=(process_queue(),))
+    thread.start()
+    # 즉시 반환
+    return {
+        "status": "success",
+        "rssID": rssID[0][0],
+        "rssName": rssID[0][1],
+        "rssUrl": rssID[0][2],
+        "favicon": rssID[0][3],
+        "message": "new created",
+    }
+
+
+async def insertRssArticle(entry, rssID: int, rssName: str = None):
+    db_gen = get_db()
+    db = next(db_gen)
+    title = entry.title
+    description = entry.description if "description" in entry else entry.summary
+    description = " ".join(str(await htmlToPlaintext(description)).split()[:40])
+    if description == None:
         description = title
 
-      writingUrl = entry.link  # 글 링크
-      thumbnail = await findImgList(rssName, entry)  # 썸네일과 이미지 리스트 추출
-      published = entry.published if 'published' in entry else None
-
-      content = None
-      if 'content' in entry:
+    writingUrl = entry.link  # 글 링크
+    thumbnail = await findImgList(rssName, entry)  # 썸네일과 이미지 리스트 추출
+    published = entry.published_parsed if "published_parsed" in entry else None
+    # print(published)
+    content = None
+    if "content" in entry:
         content = entry.content[0]["value"]
-      # 동기적으로 데이터베이스에 삽입
-      db.execute("""
-          INSERT INTO article (
-              title, descript, date, thumbnail, imgList, titleEb, descriptEb, rssID, content, link, saved
-          ) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
-      """, (
-          title, description, published,thumbnail["thumbnail"], json.dumps(thumbnail["imgList"]), await embedding(title), await embedding(description), int(rssID[0][0]), content, writingUrl, 0
-      ))
-      db.connection.commit()  # 변경 사항을 데이터베이스에 저장
+    # 동기적으로 데이터베이스에 삽입
+    db.execute(
+        """
+      INSERT INTO article (
+          title, descript, date, thumbnail, imgList, titleEb, descriptEb, rssID, content, link, saved
+      ) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
+  """,
+        (
+            title,
+            description,
+            time.mktime(published),
+            thumbnail["thumbnail"],
+            json.dumps(thumbnail["imgList"]),
+            await embedding(title),
+            await embedding(description),
+            rssID,
+            content,
+            writingUrl,
+            0,
+        ),
+    )
+    db.connection.commit()  # 변경 사항을 데이터베이스에 저장
+    return f"Article {title} inserted"
 
-    return {"status": "success", 
-            "rssID": rssID[0][0], 
-            "rssName": rssID[0][1], 
-            "rssUrl": rssID[0][2], 
-            "favicon": rssID[0][3], 
-            "message": "new created"}
-  # except Exception as e:
-  #     return {"status": "error", "message": str(e)}
+
+# except Exception as e:
+#     return {"status": "error", "message": str(e)}
--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-#FAST API Import For Set UP
+# FAST API Import For Set UP
 from typing import List
 from typing import Optional
 from fastapi import FastAPI, Depends, HTTPException
@@ -12,11 +12,12 @@ from db.db import get_db
 from db.modules.search import searchSimilar
 from recommend.recommend import recommend
 
-#다른 경로에 있는 모듈 import
+# 다른 경로에 있는 모듈 import
 import sys, os
+
 sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
 
-conn = sqlite3.connect(os.path.abspath('db/server.db'))
+conn = sqlite3.connect(os.path.abspath("db/server.db"))
 db = conn.cursor()
 
 app = FastAPI()
@@ -37,23 +38,28 @@ def read_item(any: str):
     return {"data": data}
 
 
-
-
 @app.get("/rss/add/")
-async def insert_rss_domain(domain: Optional[str] = None, db: sqlite3.Cursor = Depends(get_db)):
+async def insert_rss_domain(
+    domain: Optional[str] = None, db: sqlite3.Cursor = Depends(get_db)
+):
     if domain:
         return await addRSS(domain, db)
     else:
         raise HTTPException(status_code=400, detail="No domain provided")
 
+
 @app.get("/article/search/")
-async def search_rss(target:str, db: sqlite3.Cursor = Depends(get_db),quantity:int = 4):
+async def search_rss(
+    target: str, db: sqlite3.Cursor = Depends(get_db), quantity: int = 4
+):
     if target:
         return await searchSimilar(target, db, quantity)
 
+
 class RecommendData(BaseModel):
-    data : List[int]
-    quantity : int
+    data: List[int]
+    quantity: int
+
 
 @app.post("/article/recommend/")
 async def search_rss(item: RecommendData, db: sqlite3.Cursor = Depends(get_db)):
