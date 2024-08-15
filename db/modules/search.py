216a4373a0b4b97deb15a637d37d269132017b4a
gamzamandu <galbimandu1126@gmail.com>
2024-08-16 00:49:39 +0900

    chore : recommend의 임베딩을 참조로 끌어오게 조정

--- db/modules/search.py
+++ db/modules/search.py
@@ -1,6 +1,7 @@
 import sqlite3
 import sys, os
 from embed.embedModel import embedModel
+from embed.embedding import embedding
 import json
 
 sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
@@ -8,7 +9,9 @@ sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
 async def searchSimilar(target:str ,db, quantity): #, db: sqlite3.Cursor
   settingData = db.execute("""select articleID, titleEb, descriptEb from article""")
   settingData = settingData.fetchall()
-  result = await embedModel(target,settingData, quantity)
+
+
+  result = await embedModel(await embedding(target),settingData, quantity)
   articleData = db.execute(f"""select  r.rssID, r.rssName, rssUrl, favicon, articleID ,title, descript, date, thumbnail, imgList, content, link 
                           from article a, rss r where articleID in ({','.join(map(str,[item[0] for item in result]))}) and r.rssID = a.rssID""")
   articles_json_list = []
--- recommend/recommend.py
+++ recommend/recommend.py
@@ -2,6 +2,7 @@ import sys, os
 sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
 
 from embed.embedding import similarity, blobToNumpy
+from embed.embedModel import embedModel
 
 async def recommend(data, db, quantity):
   ebData = []
@@ -9,20 +10,41 @@ async def recommend(data, db, quantity):
     result = db.execute("""SELECT titleEb, descriptEb FROM article WHERE articleID = ?""",[rssID]).fetchone()
     ebData.append([rssID, result[0], result[1], 0])
 
+
+  sumSimilar = 0
+
+  titleEbList = []
+  descriptEbList = []
+
   for index, (rssID, titleEb, descriptEb, weight) in enumerate(ebData):
-    for index2, (rssID2, titleEb2, descriptEb2, weight2) in enumerate(ebData):
+    titleEbList.append(await blobToNumpy(titleEb))
+    descriptEbList.append(await blobToNumpy(descriptEb))
+    ebData[index].insert(4, index)
+    del ebData[index][1]
+    del ebData[index][1]
+
+  for index, (rssID, weight, reference) in enumerate(ebData):
+    for index2, (rssID2, weight2, reference2) in enumerate(ebData):
       if index != index2:
-        titleEb = await blobToNumpy(titleEb)
-        titleEb2 = await blobToNumpy(titleEb2)
-        descriptEb = await blobToNumpy(descriptEb)
-        descriptEb2 = await blobToNumpy(descriptEb2)
+        recommend = await similarity(titleEbList[index],titleEbList[index2]) + await similarity(descriptEbList[index],descriptEbList[index2])
+        sumSimilar += recommend
+        ebData[index][1] += recommend
+        ebData[index2][1] += recommend
 
-        recommend = await similarity(titleEb,titleEb2) + await similarity(descriptEb,descriptEb2)
-        ebData[index][3] += recommend
-        ebData[index2][3] += recommend
-  for i in range(len(ebData)):
-    del ebData[i][1]
-    del ebData[i][1]
-  return sorted(ebData, key=lambda x: x[0],reverse=True)
+  ebData = sorted(ebData, key=lambda x: x[1],reverse=True)
+  return ebData
+  #임베딩값 삭제
+  print(ebData[i])
 
-#descript없으면 오류나는 거 고쳐야함
+  #관계유사도 순으로 정렬 *관계유사도 : 각 데이터끼리 유사도를 돌린 각각의 합
+  # settingData = db.execute("""select articleID, titleEb, descriptEb from article""")
+  # settingData = settingData.fetchall()
+
+  # for i in range(len(ebData)):
+  #   ranged = int((ebData[i][2]/sumSimilar)*100) #관계유사도에 따른 피드수 
+  #    ranged *= (len(ebData) - i) #가중치
+  #   embedModel(ebData[i][1],settingData,ranged)
+
+#TODO: 비율로 처리해야함
+#      저장됨 가중치
+#      % 이하 search X
