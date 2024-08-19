098222bd487e1d098c5a2b7f230ca5ccad11a3cc
obtuse-triangle <me@obtuse.kr>
2024-08-19 23:26:42 +0900

    openai api 예시 코드 추가

--- openAI/ai.py
+++ openAI/ai.py
@@ -1,6 +1,7 @@
 import os
 from dotenv import load_dotenv
 import openai
+import time
 
 load_dotenv()
 
@@ -11,35 +12,64 @@ client = openai.OpenAI(api_key=os.environ["OPENAI_API_KEY"])
 
 model = "gpt-3.5-turbo"
 instruction = """
-내가 메시지를 보낼때 다음과 같은 양식으로 질문할 것이다.
+너는 본문의 글에 대한 질문을 답변하는 인공지능 챗봇이다. 본문 내에서 답을 찾아서 적절한 답변을 하여도 좋고, 웹 검색이나 지식 베이스를 참고하여 답변해도 좋다. 답변의 길이는 500자를 넘지 않도록 하자.
 
-1. {text : "내용"}
+1. 시스템은 대화의 시작 시점에 다음과 같이 본문 내용을 HTML로 전달할 것이다.
+{text : "<h1>글 제목</h1><p>본문 내용</p>...(이어서 계속)"}
 앞으로 모든 응답들은 이 text에 담긴 내용에 관련된 질문을 답변하게 된다
 이 내용을 보냈을 때는 ok라고 응답해라
 
-2.
+2. 사용자는 다음과 같은 형식으로 질문을 전달할 것이다.
 {
-    chatID : 0,
-    question : "질문"
+    question : "질문내용",
+    selection : "선택된 텍스트"
 }
 
-앞으로 chatID가 같은 question과 너의 응답내용을 참고하여 
 {
-    chatID : 0
     answer : "답변내용"
 }
 해당 양식으로 답변해야한다.
-
-가장 중요하게 지켜아할 점은
-chatID가 다른 질문에 대해서는 절대로 어떤 경우가 있어도 영향을 받지 않는다
 """
 
 assistant = client.beta.assistants.create(
     name="user-assistant",
     instructions=instruction,
-    model="gpt-4-1106-preview",
-    # tools=[{"type":"retrieval"}]
+    model="gpt-4o",
+    tools=[{"type": "retrieval"}],
 )
+
+thread = client.beta.threads.create()
+
+## 스레드 첫 시작
+client.beta.threads.messages.create(
+    thread_id=thread.id,
+    role="system",
+    content={"text": "<h1>글 제목</h1><p>본문 내용</p>...(이어서 계속)"},
+)
+
+# 사용자의 질문 메시지
+client.beta.threads.messages.create(
+    thread_id=thread.id,
+    role="user",
+    content={"question": "질문내용", "selection": "선택된 텍스트"},
+)
+
+run = client.beta.threads.create_and_run_poll(
+    thread_id=thread.id, assistant_id=assistant.id
+)
+
+# 응답 대기 무한루프
+while run.status != "completed":
+    run = client.beta.threads.polls.get(poll_id=run.id)
+    print(run.status)
+    time.sleep(1)
+
+# 응답 메시지
+messages = client.beta.threads.messages.list(
+    thread_id=thread.id
+)
+
+print(messages)
 
 # response = openai.ChatCompletion.create(
 #     model=model,
