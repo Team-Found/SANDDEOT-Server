87032dc875fe089b487068042421973b664f4083
gamzamandu <galbimandu1126@gmail.com>
2024-11-07 23:10:00 +0900

    chore : api tag

--- routes/ai.py
+++ routes/ai.py
@@ -10,18 +10,18 @@ from controllers.ai_controller import ControllerOpenAI
 
 controller = ControllerOpenAI()
 
-@router.get("/ai/getAssistant/")
+@router.get("/ai/getAssistant/", tags=["openAI"])
 async def get_assistant():
     return await controller.get_assistant()
 
-@router.post("/ai/startTalk/")
+@router.post("/ai/startTalk/", tags=["openAI"])
 async def start_talk(item: TalkData):
     return await controller.start_talk(item)
 
-@router.get("/ai/getMessageHistory/")
+@router.get("/ai/getMessageHistory/", tags=["openAI"])
 async def get_message_history(threadID: Optional[str] = None):
     return await controller.get_message_history(threadID)
 
-@router.post("/ai/markdownFormat")
+@router.post("/ai/markdownFormat", tags=["openAI"])
 async def markdownFormat(item : MarkdownFormatData):
     return await controller.markdown_format(item.content)
--- routes/rss.py
+++ routes/rss.py
@@ -5,7 +5,7 @@ from db.db import get_db
 from services.db.AddRSS import addRSS
 router = APIRouter()
 
-@router.get("/rss/add", tags=["users"])
+@router.get("/rss/add", tags=["rss"])
 async def insert_rss_domain(domain: Optional[str] = None, db: sqlite3.Cursor = Depends(get_db)):
     if domain:
         return await addRSS(domain, db)
